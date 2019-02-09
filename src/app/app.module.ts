52c861e6ca76000a2079cd18ee04444193a8e675
SweetyTapdiya31 <46708622+SweetyTapdiya31@users.noreply.github.com>
2019-02-09 19:40:09 +0530

    register component added ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './User/Register/register.component';
+import { RegisterService } from './User/Register/register-service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [RegisterService,HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
